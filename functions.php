35e60a094d223407c6e63c5565c628db0c939118
Justin Jackson <justin@nerdnorth.com>
2016-05-10 10:56:10 -0700

    Bunch of changes

--- functions.php
+++ functions.php
@@ -1,45 +1,45 @@
 <?php
-add_action( 'after_setup_theme', 'blankslate_setup' );
-function blankslate_setup()
+add_action( 'after_setup_theme', 'justin-theme_setup' );
+function justin-theme_setup()
 {
-load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
+load_theme_textdomain( 'justin-theme', get_template_directory() . '/languages' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 global $content_width;
 if ( ! isset( $content_width ) ) $content_width = 640;
 register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'blankslate' ) )
+array( 'main-menu' => __( 'Main Menu', 'justin-theme' ) )
 );
 }
-add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' );
-function blankslate_load_scripts()
+add_action( 'wp_enqueue_scripts', 'justin-theme_load_scripts' );
+function justin-theme_load_scripts()
 {
 wp_enqueue_script( 'jquery' );
 }
-add_action( 'comment_form_before', 'blankslate_enqueue_comment_reply_script' );
-function blankslate_enqueue_comment_reply_script()
+add_action( 'comment_form_before', 'justin-theme_enqueue_comment_reply_script' );
+function justin-theme_enqueue_comment_reply_script()
 {
 if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
 }
-add_filter( 'the_title', 'blankslate_title' );
-function blankslate_title( $title ) {
+add_filter( 'the_title', 'justin-theme_title' );
+function justin-theme_title( $title ) {
 if ( $title == '' ) {
 return '&rarr;';
 } else {
 return $title;
 }
 }
-add_filter( 'wp_title', 'blankslate_filter_wp_title' );
-function blankslate_filter_wp_title( $title )
+add_filter( 'wp_title', 'justin-theme_filter_wp_title' );
+function justin-theme_filter_wp_title( $title )
 {
 return $title . esc_attr( get_bloginfo( 'name' ) );
 }
-add_action( 'widgets_init', 'blankslate_widgets_init' );
-function blankslate_widgets_init()
+add_action( 'widgets_init', 'justin-theme_widgets_init' );
+function justin-theme_widgets_init()
 {
 register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'blankslate' ),
+'name' => __( 'Sidebar Widget Area', 'justin-theme' ),
 'id' => 'primary-widget-area',
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => "</li>",
@@ -47,15 +47,15 @@ register_sidebar( array (
 'after_title' => '</h3>',
 ) );
 }
-function blankslate_custom_pings( $comment )
+function justin-theme_custom_pings( $comment )
 {
 $GLOBALS['comment'] = $comment;
 ?>
 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
 <?php 
 }
-add_filter( 'get_comments_number', 'blankslate_comments_number' );
-function blankslate_comments_number( $count )
+add_filter( 'get_comments_number', 'justin-theme_comments_number' );
+function justin-theme_comments_number( $count )
 {
 if ( !is_admin() ) {
 global $id;
