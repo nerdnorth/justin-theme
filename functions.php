34f87e64ff088ab0eab82f49e2c8433dac3af33b
Justin Jackson <justin@nerdnorth.com>
2016-05-13 14:12:48 -0700

    New fork of PJRVS

--- functions.php
+++ functions.php
@@ -1,67 +1,148 @@
 <?php
-add_action( 'after_setup_theme', 'justin-theme_setup' );
-function justin-theme_setup()
-{
-load_theme_textdomain( 'justin-theme', get_template_directory() . '/languages' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-global $content_width;
-if ( ! isset( $content_width ) ) $content_width = 640;
+
+remove_action('wp_head', 'feed_links_extra', 3 );
+remove_action('wp_head', 'feed_links', 2 );
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action('wp_head', 'start_post_rel_link', 10, 0 );
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0 );
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head','jetpack_og_tags');
+if ( ! isset( $content_width ) ) $content_width = 600;
+
+
+add_theme_support('automatic-feed-links');
+add_filter('show_admin_bar', '__return_false');
+
+
+
+
+function theme_styles() {
+  wp_register_style( 'google-fonts', 'http://fonts.googleapis.com/css?family=Merriweather|Open+Sans+Condensed:400', array(), '20130425', 'all' );
+  wp_register_style( 'normalize-style', get_template_directory_uri() . '/normalize.css', array(), '20130425', 'all' );
+  wp_register_style( 'main-style', get_template_directory_uri() . '/style.css', array(), '20130425', 'all' );
+
+  wp_enqueue_style( 'google-fonts' );
+  wp_enqueue_style( 'normalize-style' );
+  wp_enqueue_style( 'main-style' );
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
+
+// MENU OPTIONS
+
+function register_my_menus() {
 register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'justin-theme' ) )
+array(
+'header-menu' => __( 'Header Menu' ),
+'footer-menu' => __( 'Footer Menu' ))
 );
 }
-add_action( 'wp_enqueue_scripts', 'justin-theme_load_scripts' );
-function justin-theme_load_scripts()
-{
-wp_enqueue_script( 'jquery' );
+
+add_action( 'init', 'register_my_menus' );
+
+
+// FEATURE IMAGE SUPPORT
+
+add_theme_support( 'post-thumbnails', array( 'post', 'page' ) ); //
+
+
+// THEME OPTIONS
+
+add_action('after_setup_theme','ttext_setup');
+function ttext_setup() {
 }
-add_action( 'comment_form_before', 'justin-theme_enqueue_comment_reply_script' );
-function justin-theme_enqueue_comment_reply_script()
-{
-if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
+
+add_action ('admin_menu', 'ttext_admin');
+
+function ttext_admin() {
+	add_theme_page( 'Customize', 'Customize', 'edit_theme_options', 'customize.php' );
 }
-add_filter( 'the_title', 'justin-theme_title' );
-function justin-theme_title( $title ) {
-if ( $title == '' ) {
-return '&rarr;';
-} else {
-return $title;
+
+
+add_action('customize_register', 'ttext_customize');
+function ttext_customize ($wp_customize) {
+
+	$wp_customize->add_section( 'ttext_settings', array(
+		'title'          => 'Justin Blog Theme Customizations',
+		'priority'       => 5,
+	) );
+
+	$wp_customize->add_setting( 'back_setting', array( 'default' => 'Return to List' ) );
+	$wp_customize->add_control( 'back_setting', array(
+		'label'   => 'Back Text',
+		'section' => 'ttext_settings',
+		'type'    => 'text'
+	) );
+
+	$wp_customize->add_setting( 'ga_setting', array( 'default' => 'UA-1421028-69' ) );
+	$wp_customize->add_control( 'ga_setting', array(
+		'label'   => 'Google Analytics ID',
+		'section' => 'ttext_settings',
+		'type'    => 'text'
+	) );
+
+return $wp_customize;
+
 }
+
+
+
+
+function colorize_save_post() {
+	global $colourvar;
+	$colourvar++;
 }
-add_filter( 'wp_title', 'justin-theme_filter_wp_title' );
-function justin-theme_filter_wp_title( $title )
-{
-return $title . esc_attr( get_bloginfo( 'name' ) );
+add_action( 'save_post', 'colorize_save_post' );
+
+
+
+
+function filter_handler( $data , $postarr ) {
+	$colorCode = get_post_meta($postarr['ID'],'colorCode');
+	if($colorCode || $data['post_status'] == 'draft') {
+		return $data;
+	}
+
+	$myposts = get_posts('numberposts=1&orderby=date&order=DESC');
+
+	foreach($myposts as $post) {
+		if ($post->ID == $postarr['ID']) {
+		} else {
+			$colorCode = get_post_meta($post->ID, 'colorCode');
+			if (!$colorCode) {
+				$colorCode = 0;
+			} else {
+				$colorCode = $colorCode[0];
+			}
+		}
+	}
+
+	$colorCodeNums = array(3, 5, 7, 11, 13);
+	$nextColorCode = 3;
+	$iterator = 0;
+
+	foreach($colorCodeNums as $colorCodeNum) {
+		$iterator++;
+		if ($colorCodeNum == $colorCode) {
+
+			if ($iterator == count($colorCodeNums)) {
+				$iterator = 0;
+			}
+			$nextColorCode = $colorCodeNums[$iterator];
+			break;
+		}
+	}
+
+	$colorCode = get_post_meta($postarr['ID'],'colorCode');
+	if(!$colorCode) {
+		add_post_meta($postarr['ID'], 'colorCode', $nextColorCode);
+	}
+
+	return $data;
 }
-add_action( 'widgets_init', 'justin-theme_widgets_init' );
-function justin-theme_widgets_init()
-{
-register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'justin-theme' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
-function justin-theme_custom_pings( $comment )
-{
-$GLOBALS['comment'] = $comment;
+add_filter( 'wp_insert_post_data', 'filter_handler', '99', 2 );
+
+
 ?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php 
-}
-add_filter( 'get_comments_number', 'justin-theme_comments_number' );
-function justin-theme_comments_number( $count )
-{
-if ( !is_admin() ) {
-global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
-}
--- index.php
+++ index.php
@@ -1,10 +1,127 @@
-<?php get_header(); ?>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+	$ga = get_theme_mod( 'ga_setting' );
+	$bback = get_theme_mod( 'back_setting' );
+	$abt = get_theme_mod( 'abt_setting' );
+	$donext = get_theme_mod( 'donext_setting' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="UTF-8">
+	<title>
+		<?php if (is_home () ) { bloginfo('name'); echo ' | '; bloginfo('description'); } elseif ( is_category() ) { single_cat_title(); echo ' | by @mijustin';  } elseif (is_single() ) { single_post_title(); echo ' | by @mijustin'; } elseif (is_page() ) { single_post_title(); echo ' | by @mijustin'; } else { wp_title('',true); echo ' | by @mijustin'; } ?>
+	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<link href="/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Author Info for Google -->
+	<link href='https://plus.google.com/u/0/115056462967470089501' rel='author'/>
+	<meta name="author" content="Justin Jackson">
+	<link rel="canonical" href="http://justinjackson.ca">
+	<?php if (is_home ()) { ?>
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<?php } elseif(is_single()) { ?>
+		<meta name="description" content="<?php $excerpt = strip_tags(get_the_excerpt()); echo $excerpt; ?>">
+	<?php } ?>
+
+	<!-- Facebook Article Info -->
+	<meta property="og:image" content="http://justinjackson.ca/apple-touch-icon.png">
+	<meta property="og:title" content="Let's make some stuff">
+	<meta property="og:site_name" content="Justin Jackson">
+	<meta property="og:url" content="http://justinjackson.ca">
+	<meta property="og:description" content="Justin Jackson is a product person from Vernon, BC, Canada">
+
+	<!-- Twitter Info -->
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@mijustin">
+	<meta name="twitter:creator" content="@mijustin">
+
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo_rss('name'); ?>" href="<?php bloginfo_rss('atom_url') ?>">
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+<?php if ( !(is_single() || is_page()) ) { ?>
+<header id="top" class="clearfix">
+  <h2 id="hometop">
+    <?php bloginfo('name'); ?>
+  </h2>
+  <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+</header>
+<?php } ?>
+<article<?php if(is_single()) { ?> class="red"<?php } ?>>
+  <?php if(is_404()) { ?>
+  <h2>Sorry, but nothing exists here.</h2>
+  <p>Find something <a href="<?php echo home_url(); ?>">interesting to read</a>.</p>
+  <?php } ?>
+  <?php if(is_home()) { query_posts('posts_per_page=-1'); } while (have_posts()) : the_post(); ?>
+  <?php if (is_page()) { ?>
+  <small><strong>&#8592 <a href="<?php echo home_url(); ?>"><?php echo $bback; ?></a></strong></small>
+  <h2>
+    <?php the_title(); ?>
+  </h2>
+  <p>
+    <?php
+						if ( has_post_thumbnail() )
+						the_post_thumbnail( 'large' );
+					?>
+  </p>
+  <?php the_content(); edit_post_link('edit', ' <span class="edit">', '</span>'); ?>
+  <?php } elseif (is_single()) { ?>
+  <small><strong>&#8592 <a href="<?php echo home_url(); ?>"><?php echo $bback; ?></a></strong></small>
+  <h2 class="red">
+    <?php if(is_single() || is_page()) { the_title(); } else { ?>
+    <a href="<?php the_permalink(); ?>">
+    <?php the_title(); ?>
+    </a>
+    <?php } ?>
+  </h2>
+  <small>Written by <a href="/?page_id=<?php echo $abt; ?>">
+  <?php the_author(); ?>
+  </a> on
+  <?php the_time('F j, Y'); ?>
+  </small>
+  <p>
+    <?php
+	if ( has_post_thumbnail() )
+		the_post_thumbnail( 'large' );
+?>
+  </p>
+  <?php the_content(); edit_post_link('edit', ' <span class="edit">', '</span>'); ?>
+  <?php } else { ?>
+  <h2 class="red">
+    <?php if(is_single() || is_page()) { the_title(); } else { ?>
+    <a href="<?php the_permalink(); ?>">
+    <?php the_title(); ?>
+    </a>
+    <?php } ?>
+  </h2>
+  <?php } ?>
+  <?php endwhile; ?>
+</article>
+<footer>
+  <?php if(!is_home()) {?>
+  <p>
+    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+  </p>
+  <p><strong><a href="<?php echo home_url(); ?>"><?php echo $bback; ?></a></strong></p>
+  <?php } else { ?>
+  <p>
+    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+  </p>
+  <?php } ?>
+  <a href="http://www.notey.com/profile/mijustin#blog/6f3bb70cea3393b0c3d9187d9d10cc88"></a>
+</footer>
+<?php wp_footer(); ?>
+
+<?php if ( ! is_preview() ) { ?>
+<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', '<?php echo $ga; ?>', '<?php echo home_url(); ?>'); ga('send', 'pageview'); </script>
+<?php  } ?>
+</body>
+</html>
